6fb41a6819ac4fe5ac4db52df106430583d6eefd
VitaliyDvil <domanevskiyvsbel@yandex.ru>
2022-01-04 20:06:39 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(num) {
+    const arr = num.toString().split('');
+    arr.reverse();
+    const newArr = arr.filter((item) => item !== '-');
+    return newArr.join('');
 }
